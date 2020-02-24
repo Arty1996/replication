706063c2dab77c041437e659bbaa1edb18dde6a2
ARTEM TARASOV <56198614+Arty1996@users.noreply.github.com>
2020-02-25 00:45:39 +0300

    Update 1

--- Код для базы данных.sql
+++ Код для базы данных.sql
@@ -1,8 +1,10 @@
--- Table: "Replication"."20190603"
+-- В PostreSQL создаём таблицу с данными для 03.06.2019
 
--- DROP TABLE "Replication"."20190603";
+-- Table: "replication"."20190603"
 
-CREATE TABLE "Replication"."20190603"
+-- DROP TABLE "replication"."20190603";
+
+CREATE TABLE "replication"."20190603"
 (
     "NO" bigint NOT NULL,
     "SECCODE" text COLLATE pg_catalog."default" NOT NULL,
@@ -19,55 +21,54 @@ CREATE TABLE "Replication"."20190603"
 
 TABLESPACE pg_default;
 
-ALTER TABLE "Replication"."20190603"
+ALTER TABLE "replication"."20190603"
     OWNER to postgres;
 
--- Далее импортируем данные из файла
-
---\copy "Replication"."20190603" FROM 'C:\Users\taras_000\Desktop\Вышка\Репликация\se\OrderLog20190603.txt' DELIMITER ',' CSV HEADER;
--- Почему-то эта команда не работает, поэтому пришлось импортировать вручную
+-- Далее импортируем данные из файла OrderLog20190603.txt с параметрами DELIMITER ',' CSV HEADER
 
 -- Удаляем лишние данные и добавляем дату
+-- В связи с тем, что объём данных слишком большой, оставим только 11 тикеров, чьи средние объёмы по торгам превысили 1 млрд руб.
 
-DELETE FROM "Replication"."20190603"
-WHERE "SECCODE" NOT IN ('AFKS', 'AFLT', 'ALRS', 'CBOM', 'CHMF', 'DSKY', 'FEES', 'FIVE', 'GAZP', 'GMKN', 'HYDR', 'IRAO', 'LKOH', 'LNTA', 'MAGN', ' MGNT', 'MOEX', 'MTSS', 'MVID', 'NLMK', 'NVTK', 'PHOR', 'PIKK', 'PLZL', 'POLY', 'RNFT', 'ROSN', 'RTKM', 'RUAL', 'SBER', 'SBERP', 'SFIN', 'SNGS', 'SNGSP', 'TATN', 'TATNP', 'TRMK', 'TRNFP', 'UPRO', 'VTBR', 'YNDX');
+ALTER TABLE "replication"."20190603" DROP "TRADENO", DROP "TRADEPRICE";
 
-ALTER TABLE "Replication"."20190603" ADD "DATE" date;
+DELETE FROM "replication"."20190603"
+WHERE "SECCODE" NOT IN ('ALRS', 'GAZP', 'GMKN', 'IRAO', 'LKOH', ' MGNT', 'ROSN', 'SBER', 'SBERP', 'TATN', 'VTBR');
 
-UPDATE "Replication"."20190603"
+ALTER TABLE "replication"."20190603" ADD "DATE" date;
+
+UPDATE "replication"."20190603"
 SET "DATE"='2019-06-03';
 
-ALTER TABLE "Replication"."20190603" DROP "ORDERNO", DROP "TRADENO", DROP "TRADEPRICE";
-
--- Такой же шаблон используем для остальных дней
+-- Такой же скрипт используем для остальных дней
 
 -- Теперь создаём общую таблицу для всех дней
 
--- Table: "Replication"."June2019"
+-- Table: "replication"."june2019"
 
--- DROP TABLE "Replication"."June2019";
+-- DROP TABLE "replication"."june2019";
 
-CREATE TABLE "Replication"."June2019"
+CREATE TABLE "replication"."june2019"
 (
     "NO" bigint NOT NULL,
     "SECCODE" text COLLATE pg_catalog."default" NOT NULL,
     "BUYSELL" text COLLATE pg_catalog."default" NOT NULL,
     "TIME" bigint NOT NULL,
+    "ORDERNO" bigint NOT NULL,
     "ACTION" smallint NOT NULL,
     "PRICE" real NOT NULL,
     "VOLUME" bigint NOT NULL,
     "DATE" date NOT NULL,
-    CONSTRAINT "June2019_pkey" PRIMARY KEY ("NO", "DATE")
+    CONSTRAINT "june2019_pkey" PRIMARY KEY ("NO", "DATE")
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE "Replication"."June2019"
+ALTER TABLE "replication"."june2019"
     OWNER to postgres;
 
 -- Копируем в неё данные из всех остальный таблиц
 
-INSERT INTO "Replication"."June2019" SELECT * FROM "Replication"."20190603"
+INSERT INTO "replication"."june2019" SELECT * FROM "replication"."20190603"
 
--- Повсторяем это действие для каждой из 19 таблиц
+-- Повторяем это действие для каждой из 19 таблиц
 -- В итоге получаем одну таблицу со всеми необходимыми данными
